4996082eb60eb472578b4bf41a2d7862abc92cb8
13krsnaa <saxenakrishna9140@gmail.com>
2026-04-08 02:13:31 +0530

    Next() fun in middlewares

--- middlewares/server.js
+++ middlewares/server.js
@@ -4,13 +4,22 @@ const app = express();
 
 //* Basic middleware *//
 
-app.use((req, res) => {
-  let { query } = req.query;
-  console.log(query);
-  console.log("This is a middleware");
-  res.send(
-    "I am a middleware and none of your routes will work in fornt of me ",
-  );
+// app.use((req, res) => {
+//   let { query } = req.query;
+//   console.log(query);
+//   console.log("This is a middleware");
+//   res.send(
+//     "I am a middleware and none of your routes will work in fornt of me ",
+//   );
+// });
+//* Middleware with next() fun. *//
+app.use((req, res, next) => {
+  console.log("hello , you hit the route");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("Hi , i am 2nd middleware");
+  next();
 });
 
 //* basic requests *//
