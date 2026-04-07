08207c3a9f0ccba03793dbaa6070618e33851d68
13krsnaa <saxenakrishna9140@gmail.com>
2026-04-08 02:39:49 +0530

    logger middleware

--- middlewares/server.js
+++ middlewares/server.js
@@ -12,17 +12,33 @@ const app = express();
 //     "I am a middleware and none of your routes will work in fornt of me ",
 //   );
 // });
-//* Middleware with next() fun. *//
+
+// //* Middleware with next() fun. *//
+
+// app.use((req, res, next) => {
+//   console.log("hello , you hit the route");
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.log("Hi , i am 2nd middleware");
+//   next();
+// });
+
+//* Utility Middleware *//
+
+// ye logger middleware hai jo web req ko log krta hai
+
 app.use((req, res, next) => {
-  console.log("hello , you hit the route");
-  next();
-});
-app.use((req, res, next) => {
-  console.log("Hi , i am 2nd middleware");
+  req.time = new Date(Date.now()).toString();
+  console.log(`Request received at : ${req.time} 
+Request method: ${req.method} 
+Request path: ${req.path} 
+request hostname: ${req.hostname}`);
   next();
 });
 
 //* basic requests *//
+
 app.get("/", (req, res) => {
   res.send("Hii, i am homepage");
 });
@@ -31,6 +47,7 @@ app.get("/random", (req, res) => {
   res.send("this is a random page");
 });
 
+//* Server Listening on port 3000*//
 app.listen(3000, () => {
   console.log("Server is runing on port 3000");
 });
